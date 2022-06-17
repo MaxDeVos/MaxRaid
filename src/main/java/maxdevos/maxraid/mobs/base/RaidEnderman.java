874ba8b9ebc9e4317bf42ec4617ff77f01307875
Max DeVos <devosmaxwell@gmail.com>
2022-06-17 16:59:28 -0500

    RaidEnderman added (poorly)

--- src/main/java/maxdevos/maxraid/mobs/base/RaidEnderman.java
+++ src/main/java/maxdevos/maxraid/mobs/base/RaidEnderman.java
@@ -6,8 +6,9 @@ import maxdevos.maxraid.goals.NearestAttackableMaxRaidTargetGoal;
 import maxdevos.maxraid.raid.MaxRaid;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
@@ -40,18 +41,19 @@ public class RaidEnderman extends CraftEnderman {
             targetSelector.removeAllGoals();
         }
 
+        /** Currently mostly random, will attack villagers and randomly meander around.
+         *  Pretty ineffective in battle at the moment, to be honest. Has much potential. */
         protected void registerRaidGoals() {
-            //TODO make this mob-specific
+
             goalSelector.addGoal(1, new FloatGoal(this));
-//            goalSelector.addGoal(2, new ZombieAttackGoal(this, 2.0, true));
+            goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0, false));
             goalSelector.addGoal(3, new MoveTowardsPointGoal(this, raid.getVillageCenter(), 1.0));
             goalSelector.addGoal(4, new LookAtPointGoal(this, raid.getVillageCenter()));
 
             targetSelector.addGoal(1, new HurtByTargetGoal(this));
-            targetSelector.addGoal(2, new NearestAttackableMaxRaidTargetGoal<>(this, Player.class, false));
-            targetSelector.addGoal(3, new NearestAttackableMaxRaidTargetGoal<>(this, AbstractVillager.class, false));
+            targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
+            targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, true));
 
         }
     }
-
 }
